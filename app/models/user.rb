2aedd7179c0ef13c27a1720cc0d3c15a65169c34
JEFF <justaboutjeff@gmail.com>
2013-06-19 16:25:06 -0500

    refactor tweet method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   has_many :tweets
-  
+
   validates :username,     presence: true
   validates :oauth_token,  presence: true,
                            uniqueness: true
   validates :oauth_secret, presence: true,
                            uniqueness: true
 
-  def tweet(params)
+  def tweet(params={})
     params[:timer] ||= 0
     tweet = Tweet.create!(:body => params[:body], user_id: self.id)
     TweetWorker.perform_in(params[:timer].to_i.minutes, tweet.id)
